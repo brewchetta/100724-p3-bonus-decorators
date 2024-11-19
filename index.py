b028587a9690cd34230450bf1760f539b8fb3fda
brewchetta <chett.tiller@gmail.com>
2024-11-19 11:47:58 -0500

    dirty birds

--- index.py
+++ index.py
@@ -1,2 +1,9 @@
 from classes.bird import Bird
 from classes.fish import Fish
+
+cardinal = Bird(name="cardinal")
+woodpecker = Bird(name="woodpecker")
+yellow_breasted_tit = Bird(name="yellow breasted tit")
+blue_footed_booby = Bird(name="blue footed booby", category="water")
+egret = Bird(name="egret", category="water")
+egret.BRAIN_SIZE = 'gigantic' # overwrites the constant JUST for the very smart egret
